85b63e6d3a75c097985472042aa4c9778db64092
skiddyt <vincent.mendel@gmx.de>
2020-10-13 15:24:53 +0200

    changes to itemcomponents, still errors

--- src/de/taron10lp/rust/itemstacks/ItemComponents.java
+++ src/de/taron10lp/rust/itemstacks/ItemComponents.java
@@ -1,6 +1,7 @@
 package de.taron10lp.rust.itemstacks;
 
 import de.taron10lp.rust.main.Rust;
+import net.minecraft.server.v1_16_R1.Items;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -11,18 +12,58 @@ public class ItemComponents {
 
     public ItemComponents() {}
 
-    public ItemStack getPipe(int amount) {
-        ItemStack pipe = new ItemStack(Material.BAMBOO);
-        pipe.setAmount(amount);
-        ItemMeta pipeMeta = pipe.getItemMeta();
-        pipeMeta.setDisplayName("Pipe");
-        pipe.setItemMeta(pipeMeta);
+    public ItemStack getMetalPipe(int amount) {
+        ItemStack metalPipe = new ItemStack(Material.BAMBOO);
+        metalPipe.setAmount(amount);
+        ItemMeta metalPipeMeta = metalPipe.getItemMeta();
+        metalPipeMeta.setDisplayName("Metal Pipe");
+        metalPipe.setItemMeta(metalPipeMeta);
 
-        return pipe;
+        return metalPipe;
     }
     public ItemStack getSewingKit(int amount) {
         ItemStack sewingKit = new ItemStack(Material.LEAD);
+        sewingKit.setAmount(amount);
+        ItemMeta sewingKitMeta = sewingKit.getItemMeta();
+        sewingKitMeta.setDisplayName("Sewing Kit");
+        sewingKit.setItemMeta(sewingKitMeta);
 
         return sewingKit;
+    }
+    public ItemStack getGears(int amount) {
+        ItemStack gears = new ItemStack(Material.MUSIC_DISC_11);
+        gears.setAmount(amount);
+        ItemMeta gearsMeta = gears.getItemMeta();
+        gearsMeta.setDisplayName("Gears");
+        gears.setItemMeta(gearsMeta);
+
+        return gears;
+    }
+    public ItemStack getMetalBlade(int amount) {
+        ItemStack metalBlade = new ItemStack(Material.MUSIC_DISC_11);
+        metalBlade.setAmount(amount);
+        ItemMeta metalBladeMeta = metalBlade.getItemMeta();
+        metalBladeMeta.setDisplayName("Metal Blade");
+        metalBlade.setItemMeta(metalBladeMeta);
+
+        return metalBlade;
+    }
+    public ItemStack getMetalSpring(int amount) {
+        ItemStack metalSpring = new ItemStack(Material.CHAIN);
+        metalSpring.setAmount(amount);
+        ItemMeta metalSpringMeta = metalSpring.getItemMeta();
+        metalSpringMeta.setDisplayName("Metal Spring");
+        metalSpring.setItemMeta(metalSpringMeta);
+
+        return metalSpring;
+    }
+    public ItemStack getMetalSpring(int amount) {
+        ItemStack metalSpring = new ItemStack(Material.CHAIN);
+        metalSpring.setAmount(amount);
+        ItemMeta metalSpringMeta = metalSpring.getItemMeta();
+        metalSpringMeta.setDisplayName("Metal Spring");
+        metalSpring.setItemMeta(metalSpringMeta);
+
+        return metalSpring;
     }
 }
